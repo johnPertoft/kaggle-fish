f5a5d1f82e420d34b741fcc9d25bfcb96c452340
John Pertoft <john.pertoft@gmail.com>
2016-11-21 23:05:42 +0100

    Add temporary model

--- model.py
+++ model.py
@@ -1,12 +1,68 @@
 import tensorflow as tf
 import numpy as np
 
-def conv2d(prev, shape, scope_name):
-    with tf.variable_scope(scope_name) as scope:
-       pass 
+def conv2d(prev, name, shape, stride=(1, 1, 1, 1)):
+    with tf.variable_scope(name) as scope:
+        kernel = tf.Variable(tf.truncated_normal(shape, dtype=tf.float32, stddev=1e-1), 
+                             name="weights")
+        
+        conv = tf.nn.conv2d(prev, kernel, stride, padding="SAME")
+
+        biases = tf.Variable(tf.constant(0.0, shape=[shape[-1]], dtype=tf.float32),
+                             name="biases")
+
+        bias_add = tf.nn.bias_add(conv, biases)
+
+        return tf.nn.relu(bias_add, name=name)
+
+def max_pool2d(prev, name, shape, stride=[1, 2, 2, 1]):
+    return tf.nn.max_pool(prev, shape, stride, padding="SAME", name=name) 
+
+def dense(prev, name, output_dim, activation=True):
+    with tf.variable_scope(name) as scope:
+        flatten_first = len(prev.get_shape().as_list()) > 2
+        if flatten_first:
+            print(prev.get_shape().as_list())
+            shape = np.prod(prev.get_shape().as_list()[1:])
+            print(shape)
+            prev = tf.reshape(prev, [-1, shape])
+        else:
+            shape = prev.get_shape().as_list()[1]
+
+        weights = tf.Variable(tf.truncated_normal((shape, output_dim), 
+                                                  dtype=tf.float32, 
+                                                  stddev=1e-1),
+                              name="weights")
+        
+        biases = tf.Variable(tf.constant(1.0, shape=[output_dim], dtype=tf.float32),
+                             name="biases")
+
+        bias_add = tf.nn.bias_add(tf.matmul(prev, weights), biases)
+        
+        return tf.nn.relu(bias_add, name=name) if activation else bias_add 
 
 class Fishmodel:
-    def __init__(self, X):
-        # TODO: smart model
-        self.foo = tf.Variable(tf.constant(1))
-
+    def __init__(self, X, num_classes):
+        # TODO: just some random model to start with atm
+        # TODO: add dropout, tf.nn.dropout as keep prob as tf.placeholder
+        
+        self.X = X
+        
+        self.conv1 = conv2d(self.X, "conv1", shape=(3, 3, 3, 16))
+        self.conv2 = conv2d(self.conv1, "conv2", shape=(3, 3, 16, 32))
+        self.mpool1 = max_pool2d(self.conv2, 
+                                 "max_pool1", 
+                                 shape=(1, 2, 2, 1), 
+                                 stride=(1, 2, 2, 1))
+        
+        self.conv3 = conv2d(self.mpool1, "conv3", shape=(3, 3, 32, 64))
+        self.conv4 = conv2d(self.conv3, "conv4", shape=(3, 3, 64, 64))
+        self.mpool2 = max_pool2d(self.conv4, 
+                                 "max_pool2", 
+                                 shape=(1, 2, 2, 1), 
+                                 stride=(1, 2, 2, 1))
+        
+        self.dense1 = dense(self.mpool2, "dense1", 2048)
+        self.dense2 = dense(self.dense1, "dense2", 2048)
+        self.logits = dense(self.dense2, "logits", num_classes, activation=False)
+        self.softmax = tf.nn.softmax(self.logits, name="softmax") 
--- train.py
+++ train.py
@@ -8,6 +8,7 @@ import datetime
 from skimage.io import imread
 from skimage.transform import resize
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.utils import shuffle
 import tensorflow as tf
 import numpy as np
 
@@ -35,7 +36,7 @@ def load_train_data(training_rootdir):
             y_train.append(i)
 
     x_train = np.array(x_train)
-    y_train = OneHotEncoder().fit_transform(np.array(y_train)[:, np.newaxis])
+    y_train = OneHotEncoder(sparse=False).fit_transform(np.array(y_train)[:, np.newaxis])
 
     rgb_mean = x_train.mean(axis=(0, 1, 2)) # TODO: Maybe remove per image instead?
     x_train = preprocess(x_train, rgb_mean)
@@ -45,6 +46,14 @@ def load_train_data(training_rootdir):
 def load_test_data(test_dir, rgb_mean):
     x_test = [read_img(p) for p in glob.glob(os.path.join(test_dir, "*.jpg"))]
     return preprocess(np.array(x_test), rgb_mean)
+
+def batch_generator(x_train, y_train, batch_size):
+    N = x_train.shape[0]
+    while True:
+        x_train, y_train = shuffle(x_train, y_train)
+        for i in range(0, N, batch_size):
+            j = min(i + batch_size, N)
+            yield x_train[i:j], y_train[i:j]
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
@@ -58,19 +67,24 @@ if __name__ == "__main__":
     x_train, y_train, rgb_mean = load_train_data(os.path.join(args.dataset_dir, "train"))
     x_test = load_test_data(os.path.join(args.dataset_dir, "test_stg1"), rgb_mean)
     print("Loaded data.")
-    
+  
     with tf.Session() as sess:
-        # Create the model 
-        X = tf.placeholder(tf.float32, (None, IMG_SHAPE[0], IMG_SHAPE[1]))
-        fish_model = Fishmodel(X)
+        # Create the model
+        X = tf.placeholder(tf.float32, (None, IMG_SHAPE[0], IMG_SHAPE[1], 3))
+        target = tf.placeholder(tf.float32, (None, 8))
+        fish_model = Fishmodel(X, num_classes=8)
         
+        # Cross entropy loss
+        loss = tf.nn.softmax_cross_entropy_with_logits(fish_model.logits, target)
+
         # Summary reports for tensorboard
         merged_summary = tf.merge_all_summaries()
         summary_writer = tf.train.SummaryWriter(SUMMARY_DIR, sess.graph)
 
         saver = tf.train.Saver(tf.all_variables())
-        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
         model_savename = args.run_name if args.run_name else timestamp
+        print("Model savename: ", model_savename)
         
         # Save model on ctrl-c
         def exit_handler(signal, frame):
@@ -81,5 +95,10 @@ if __name__ == "__main__":
         print("Starting training.")
         print("Exit with CTRL-C, model will be saved on exit.")
 
+        train_step = tf.train.AdamOptimizer(1e-3).minimize(loss)
+        batches = batch_generator(x_train, y_train, batch_size=128)
+        sess.run(tf.initialize_all_variables())
         while True:
-            pass
+            x_batch, y_batch = next(batches)
+            train_step.run(feed_dict={X: x_batch, target: y_batch})
+            # TODO: write accuracy to summary write sometimes
